faf1e32bbebe4e24256baeb89bfc85bb44b72efa
Dmitry Gusev <dmtrxiii@gmail.com>
2022-05-23 12:06:26 +0300

    activity instant state saving released

--- app/src/main/java/com/ineedyourcode/westcoastcustomsintentservice/MainActivity.kt
+++ app/src/main/java/com/ineedyourcode/westcoastcustomsintentservice/MainActivity.kt
@@ -10,6 +10,15 @@ import com.ineedyourcode.westcoastcustomsintentservice.databinding.ActivityMainB
 import com.ineedyourcode.westcoastcustomsintentservice.intentservice.CustomTimerService
 import com.ineedyourcode.westcoastcustomsintentservice.intentservice.TimerService
 
+private const val INTENT_SERVICE_TIMER_VALUE_ARG_KEY =
+    "INTENT_SERVICE_TIMER_VALUE_ARG_KEY"
+private const val CUSTOM_INTENT_SERVICE_TIMER_VALUE_ARG_KEY =
+    "CUSTOM_INTENT_SERVICE_TIMER_VALUE_ARG_KEY"
+private const val START_SERVICE_BUTTON_STATE_ARG_KEY =
+    "START_SERVICE_BUTTON_STATE_ARG_KEY"
+private const val STOP_SERVICE_BUTTON_STATE_ARG_KEY =
+    "STOP_SERVICE_BUTTON_STATE_ARG_KEY"
+
 class MainActivity : AppCompatActivity() {
     companion object {
         const val ACTION_TIMER_TIME = "ACTION_TIMER_TIME"
@@ -60,6 +69,7 @@ class MainActivity : AppCompatActivity() {
     private fun stopServices() {
         stopService(timerServiceIntent)
         stopService(customTimerServiceIntent)
+        binding.stopAllServicesButton.isEnabled = false
     }
 
     private fun startServices() {
@@ -75,6 +85,7 @@ class MainActivity : AppCompatActivity() {
 
         if (intent?.extras?.containsKey(TIMER_SERVICE_IS_STOPPED_EXTRA_KEY) == true) {
             binding.startAndroidIntentServiceButton.isEnabled = true
+            binding.stopAllServicesButton.isEnabled = true
         }
 
         if (intent?.extras?.containsKey(CUSTOM_TIMER_SERVICE_VALUE_EXTRA_KEY) == true) {
@@ -84,7 +95,34 @@ class MainActivity : AppCompatActivity() {
 
         if (intent?.extras?.containsKey(CUSTOM_TIMER_SERVICE_IS_STOPPED_EXTRA_KEY) == true) {
             binding.startAndroidIntentServiceButton.isEnabled = true
+            binding.stopAllServicesButton.isEnabled = true
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.apply {
+            putString(INTENT_SERVICE_TIMER_VALUE_ARG_KEY,
+                binding.timerForAndroidIntentServiceTextView.text.toString())
+            putString(CUSTOM_INTENT_SERVICE_TIMER_VALUE_ARG_KEY,
+                binding.timerForCustomIntentServiceTextView.text.toString())
+            putBoolean(START_SERVICE_BUTTON_STATE_ARG_KEY,
+                binding.startAndroidIntentServiceButton.isEnabled)
+            putBoolean(STOP_SERVICE_BUTTON_STATE_ARG_KEY,
+                binding.stopAllServicesButton.isEnabled)
+        }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.timerForAndroidIntentServiceTextView.text =
+            savedInstanceState.getString(INTENT_SERVICE_TIMER_VALUE_ARG_KEY).toString()
+        binding.timerForCustomIntentServiceTextView.text =
+            savedInstanceState.getString(CUSTOM_INTENT_SERVICE_TIMER_VALUE_ARG_KEY).toString()
+        binding.startAndroidIntentServiceButton.isEnabled =
+            savedInstanceState.getBoolean(START_SERVICE_BUTTON_STATE_ARG_KEY)
+        binding.stopAllServicesButton.isEnabled =
+            savedInstanceState.getBoolean(STOP_SERVICE_BUTTON_STATE_ARG_KEY)
     }
 
     override fun onDestroy() {
